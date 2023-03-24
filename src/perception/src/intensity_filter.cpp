8660bf631866920c8b805e35c99651dae95a20eb
ahmed bakr <ambakr@crimson.ua.edu>
2023-03-24 10:33:47 -0700

    - Fixed the compilation problems inside `intensity_filter.cpp`

--- src/perception/src/intensity_filter.cpp
+++ src/perception/src/intensity_filter.cpp
@@ -10,34 +10,36 @@
 #include <pcl/filters/passthrough.h>
 
 // ROS
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <ros/ros.h>
+#include <std_msgs/String.h>
 #include "sensor_msgs/PointCloud2.h"
 #include "pcl_conversions/pcl_conversions.h"
 
 using namespace std::chrono_literals;
 
+
 class MinimalPublisher : public ros::NodeHandle
 {
 public:
     MinimalPublisher()
-            : Node("minimal_publisher"), count_(0)
+            : ros::NodeHandle("minimal_publisher"), count_(0)
     {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        publisher_ = this->advertise<std_msgs::String>("topic", 10); //create a publisher
         timer_ = this->createWallTimer(
-                500ms, std::bind(&MinimalPublisher::timer_callback, this));
+                ros::WallDuration(0.5), std::bind(&MinimalPublisher::timer_callback, this));
+    }
+
+    void timer_callback()
+    {
+        auto message = std_msgs::String();
+        message.data = "Hello, world! " + std::to_string(count_++); 
+        ROS_INFO_STREAM("Publishing: " + message.data);
+        publisher_.publish(message);
     }
 
 private:
-    void timer_callback()
-    {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, world! " + std::to_string(count_++);
-        ROS_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        publisher_->publish(message);
-    }
-    ros::Timer::SharedPtr timer_;
-    ros::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    ros::WallTimer timer_;
+    ros::Publisher publisher_;
     size_t count_;
 };
 
@@ -47,19 +49,19 @@ public:
     IntensityFilter()
         : NodeHandle("intensity_filter")
     {
-        this->declare_parameter<int>("min_intensity_threshold", 0);
-        this->declare_parameter<int>("max_intensity_threshold", 255);
-        _subscription = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+        this->param("min_intensity_threshold", 0); // declare a parameter
+        this->param("max_intensity_threshold", 255); // declare a parameter
+        _subscription = this->subscribe<sensor_msgs::PointCloud2>(
                 "/velodyne_points", 10, std::bind(&IntensityFilter::topic_callback, this, std::placeholders::_1));
-        _publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("intensity_filtered_cloud", 10);
+        _publisher = this->advertise<sensor_msgs::PointCloud2>("intensity_filtered_cloud", 10);
     }
-    void topic_callback(const sensor_msgs::msg::PointCloud2::SharedPtr rosCloud)
+    void topic_callback(const sensor_msgs::PointCloud2::ConstPtr& rosCloud)
     {
         int min_intensity_threshold, max_intensity_threshold;
-        this->get_parameter("min_intensity_threshold", min_intensity_threshold);
-        this->get_parameter("max_intensity_threshold", max_intensity_threshold);
+        this->getParam("min_intensity_threshold", min_intensity_threshold);
+        this->getParam("max_intensity_threshold", max_intensity_threshold);
 
-        ROS_INFO(this->get_logger(), "There are %zu points in the cloud", rosCloud->data.size());
+        ROS_INFO_STREAM("Number of points in the cloud: " + rosCloud->data.size());
         pcl::PointCloud<pcl::PointXYZI>::Ptr pclCloud(new pcl::PointCloud<pcl::PointXYZI>);
         pcl::fromROSMsg(*rosCloud, *pclCloud);
 
@@ -70,23 +72,24 @@ public:
         intensityFilter.filter(*pclCloud);
 
 
-        sensor_msgs::msg::PointCloud2 rosCloud2;
+        sensor_msgs::PointCloud2 rosCloud2;
 
         pcl::toROSMsg(*pclCloud, rosCloud2);
         rosCloud2.header = rosCloud->header;
-        _publisher->publish(rosCloud2);
+        _publisher.publish(rosCloud2);
     }
 
 private:
-    ros::Subscriber<sensor_msgs::msg::PointCloud2>::SharedPtr _subscription;
-    ros::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr _publisher;
+    ros::Subscriber _subscription;
+    ros::Publisher _publisher;
 };
 
 int main(int argc, char * argv[])
 {
-    ros::init(argc, argv);
+    ros::init(argc, argv, "intensity_filter");
     //rclcpp::spin(std::make_shared<MinimalPublisher>());
-    ros::spin(std::make_shared<IntensityFilter>());
+    auto intensityFilter = IntensityFilter();
+    ros::spin();
     ros::shutdown();
     return 0;
 }
